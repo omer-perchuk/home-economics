c03aab2a6f5a7773663004725f70ca503888f3b8
omer-perchuk <perchuk.omer@gmail.com>
2026-03-08 23:58:24 +0200

    add postgres support

--- app/db/database.py
+++ app/db/database.py
@@ -1,17 +1,23 @@
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./home_economics.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./data.db")
+
+connect_args = {}
+if DATABASE_URL.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    connect_args=connect_args,
+    pool_pre_ping=True,
 )
 
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
-    bind=engine
+    bind=engine,
 )
 
 Base = declarative_base()
