529d5e23fc7fc138f246a75c4237aaefc2e49506
omer-perchuk <perchuk.omer@gmail.com>
2026-03-09 13:13:45 +0200

    update frontend backend url

--- app/main.py
+++ app/main.py
@@ -15,14 +15,16 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "http://127.0.0.1:3000",
-        "http://192.168.68.101:3000",
+        "http://localhost:3001",
+        "https://home-economics-flax.vercel.app"
     ],
-    allow_credentials=False,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
